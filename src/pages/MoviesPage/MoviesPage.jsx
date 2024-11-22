51cc7c3c83b0ec475ccf29b1f43c02f379985e40
EevgenK <45nalitay45@gmail.com>
2024-11-22 16:49:00 +0200

    finaly

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect,  useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import Notification from "../../components/Notification/Notification";
